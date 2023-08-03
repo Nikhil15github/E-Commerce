f4e07dfd8993145a9d83ca638c9dfb02560b80b3
Nikhil <nikpawarnp2@gmail.com>
2023-08-04 03:51:36 +0530

    classes added

---
+++ src/com/amdocs/wipro/ecommerce/BuyProduct.java
@@ -0,0 +1,45 @@
+package com.amdocs.wipro.ecommerce;
+
+import java.util.Scanner;
+
+public class BuyProduct extends AddToCart
+{
+
+	//AddToCart price = new AddToCart();
+	
+	
+	public void buyProduct()
+	{
+		System.out.println();
+		System.out.println("Do you want to buy product");
+		
+		System.out.println("yes/No");
+		
+		Scanner sc = new Scanner(System.in);
+		String d=sc.next();
+		
+		//System.out.println(d);
+		
+		
+		
+		if(d.equals("yes"))
+			
+		{
+			
+			System.out.println("Your total ammount of bill is "+ AddToCart.productPrice);
+			
+		}
+		if(d.equals("no"))
+		{
+			System.out.println("Thank you for using application");
+		}
+		
+		
+	}
+	
+		public static void main(String[] args) 
+		{
+		BuyProduct obj= new BuyProduct();
+		obj.buyProduct();
+		}
+}
